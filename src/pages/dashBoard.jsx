4a7a190a1bd82d47eabb89827dd6848591cfad34
lalithranga <wkdlkumara@gmail.com>
2025-02-19 19:17:46 +1300

    second commit

--- src/pages/dashBoard.jsx
+++ src/pages/dashBoard.jsx
@@ -1,21 +1,22 @@
 
-
-import Header from '../component/header'
 import { Routes, Route } from 'react-router-dom'
 import Home from './home/home'
-import Contact from './contact'
-import Footer from './footer/footer'
+
+
 import NewsPage from './news/news'
+import Contact from './contact/contact'
+import Footer from '../component/footer/footer'
+import Header from '../component/header/header'
 
 export default function DashBoard() {
     return (
         <div>
-            <Header />
+            <Header/>
             <Routes path="/*">
 
                 <Route path="/" element={<Home />} />
 
-                <Route path="/contact" element={<Contact />} />
+                <Route path="/contact" element={<Contact/>} />
                 <Route path="*" element={<h1>404</h1>} />
                 <Route path="/news" element={<NewsPage />} />
               
